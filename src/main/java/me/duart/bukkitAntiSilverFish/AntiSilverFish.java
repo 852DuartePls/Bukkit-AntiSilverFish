dc4627327944af32f0a4f05feeea24908225f1d1
852DuartePls <duartebenitez_001@hotmail.com>
2024-10-29 12:27:55 -0300

    Setup

---
+++ src/main/java/me/duart/bukkitAntiSilverFish/AntiSilverFish.java
@@ -0,0 +1,12 @@
+package me.duart.bukkitAntiSilverFish;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiSilverFish extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
