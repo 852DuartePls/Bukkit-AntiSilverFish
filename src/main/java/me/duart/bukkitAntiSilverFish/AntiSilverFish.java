f04ba464a473246d3c6cace4f3f1c4e937fccbaa
852DuartePls <duartebenitez_001@hotmail.com>
2024-10-29 13:53:09 -0300

    Prevent Silverfish behavior using two events One to prevent them from spawning by breaking an Infested Block Another to prevent them from Re-Entering a block

--- src/main/java/me/duart/bukkitAntiSilverFish/AntiSilverFish.java
+++ src/main/java/me/duart/bukkitAntiSilverFish/AntiSilverFish.java
@@ -1,12 +1,34 @@
 package me.duart.bukkitAntiSilverFish;
 
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AntiSilverFish extends JavaPlugin {
+import javax.annotation.Nonnull;
+
+public final class AntiSilverFish extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+    }
 
+    // Prevent Spawning from Infested Blocks
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    public void onSilverFishSpawn(@Nonnull CreatureSpawnEvent event) {
+        if (event.getEntityType() != EntityType.SILVERFISH) return;
+        if (event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.SILVERFISH_BLOCK) return;
+        event.setCancelled(true);
+    }
+
+    // Prevent them from re-entering blocks
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    public void onSilverFishEnterBlock(@Nonnull EntityChangeBlockEvent event) {
+        if (event.getEntityType() != EntityType.SILVERFISH) return;
+        event.setCancelled(true);
     }
 }
