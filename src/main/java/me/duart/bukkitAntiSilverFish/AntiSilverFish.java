22925f324e1642bc5d3d1108d67ab333c344d662
852DuartePls <duartebenitez_001@hotmail.com>
2025-09-13 20:32:11 -0300

    Cache config, read only on load or reload

--- src/main/java/me/duart/bukkitAntiSilverFish/AntiSilverFish.java
+++ src/main/java/me/duart/bukkitAntiSilverFish/AntiSilverFish.java
@@ -16,9 +16,14 @@ import javax.annotation.Nonnull;
 
 public final class AntiSilverFish extends JavaPlugin implements Listener {
 
+    private volatile boolean disableSpawning;
+    private volatile boolean disableReenter;
+    private volatile boolean disablePotion;
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
+        loadConfig();
         ReloadCommand reloadCommand = new ReloadCommand(this);
 
         PluginCommand pluginCommand = getCommand("antisilverfish");
@@ -32,6 +37,13 @@ public final class AntiSilverFish extends JavaPlugin implements Listener {
 
     public void onReload() {
         reloadConfig();
+        loadConfig();
+    }
+
+    private void loadConfig() {
+        disableSpawning = getConfig().getBoolean("Disable_Silverfish_Spawning", true);
+        disableReenter  = getConfig().getBoolean("Disable_Silverfish_Reentering", true);
+        disablePotion   = getConfig().getBoolean("Disable_Infestation_Potions", true);
     }
 
     // Prevent Spawning from Infested Blocks
@@ -39,7 +51,7 @@ public final class AntiSilverFish extends JavaPlugin implements Listener {
     public void onSilverFishSpawn(@Nonnull CreatureSpawnEvent event) {
         if (event.getEntityType() != EntityType.SILVERFISH) return;
         if (event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.SILVERFISH_BLOCK) return;
-        if (!getConfig().getBoolean("Disable_Silverfish_Spawning", true)) return;
+        if (!disableSpawning) return;
         event.setCancelled(true);
     }
 
@@ -47,14 +59,14 @@ public final class AntiSilverFish extends JavaPlugin implements Listener {
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
     public void onSilverFishEnterBlock(@Nonnull EntityChangeBlockEvent event) {
         if (event.getEntityType() != EntityType.SILVERFISH) return;
-        if (!getConfig().getBoolean("Disable_Silverfish_Reentering", true)) return;
+        if (!disableReenter) return;
         event.setCancelled(true);
     }
 
     // Prevent the Infestation potion from working
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
     public void onSilverFishInfestation(@Nonnull EntityPotionEffectEvent event) {
-        if (!getConfig().getBoolean("Disable_Infestation_Potions", true)) return;
+        if (!disablePotion) return;
         if (event.getCause() != EntityPotionEffectEvent.Cause.ARROW
                 && event.getCause() != EntityPotionEffectEvent.Cause.POTION_SPLASH
                 && event.getCause() != EntityPotionEffectEvent.Cause.AREA_EFFECT_CLOUD) return;
